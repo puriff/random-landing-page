99d5bf11c43f0f7c7204987688a202f18fb6b40f
purif <purifavax@protonmail.com>
2023-01-01 01:36:33 +0100

    general update

--- src/Main.js
+++ src/Main.js
@@ -1,6 +1,9 @@
-import { Button } from "@mui/material";
+import { Button, Divider, List, ListItem, ListItemIcon } from "@mui/material";
 import styled, { ThemeProvider } from "styled-components";
 import happy from "./happy.png";
+import CircleIcon from '@mui/icons-material/Circle';
+import logo from './logo.png'
+import abstract from './abstract.png'
 
 const Container = styled.div`
   position: relative;
@@ -14,8 +17,8 @@ const Container = styled.div`
     width: 90%;
     height: 70%;
     top: 5%;
-    border: 1px solid white;
     margin: auto;
+    margin-bottom: 5%;
 
         .text-div {
             position: relative;
@@ -89,11 +92,111 @@ const Container = styled.div`
 
     .list-div {
         position: relative;
-        height: 100%;
+        height: fit-content;
         width: 90%;
         margin: auto;
-        background: red;
+        color: white;
+
+        .special-text-color {
+            color: #3DE1B8;
+        }
+
+        .underlined-text {
+            color: #3DE1B8;
+            text-decoration: #3DE1B8 underline;
+        }
+
+        .list {
+
+            .list-item {
+                margin-bottom: 15px;
+                font-size: 14px;
+
+                .list-item-icon {
+                    background-color:  #3DE1B8;
+                    border-radius: 50%;
+                    -webkit-box-shadow:0px 0px 62px 13px rgba(45,255,196,0.57);
+                    -moz-box-shadow: 0px 0px 62px 13px rgba(45,255,196,0.57);
+                    box-shadow: 0px 0px 10px 2px #3DE1B8;
+                }
+            }
+        }
     }
+
+    .bottom-part {
+        position: relative;
+        width: 100%;
+        height: 70%;
+        margin: auto;
+        margin-top: 5%;
+
+        .blurred-part {
+            height: 30%;
+            width: 80%;
+            border: 1px solid white;
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            border-radius: 15px;
+            margin: auto;
+            margin-bottom: 10%;
+            
+            .text-part{
+                .title-blurred {
+                    color: #3DE1B8;
+                    width: fit-content;
+                    position: relative;
+                    margin-top: 0;
+                    margin-bottom: 0;
+                    margin-left: 2%;
+                }
+
+                .text-blurred {
+                    color: white;
+                    margin-left: 2%;
+                    width: 60%;
+                    font-size: 14px;
+                }
+            }
+
+            .img-blurred {
+                position: relative;
+                height: 60%;
+                right: 2%;
+                
+                
+            }
+        }
+
+        .bottom-text {
+            display: flex;
+            positon: relative;
+            width: 90%;
+            height: 20%;
+            margin: auto;
+            justify-content: center;
+            align-items: center;
+
+            .bottom-text-h1 {
+                color: white;
+
+                .special-text-color-underlined {
+                    color: #3DE1B8;
+                    text-decoration: #3DE1B8 underline;
+                }
+            }
+        }
+
+        .bottom-image {
+            position: absolute;
+            width: 50%;
+            bottom: 0;
+            right: -10%;
+        }
+    }
+
+
+
 
   @media only screen and (max-width: 768px) {
     
@@ -177,7 +280,52 @@ function openLink(link) {
                 <img src={happy} className="image-apu"></img>
             </div>
         </div>
-        <div className="list-div"></div>       
+        <div className="list-div">
+            <h1 className="title">Special <span className="special-text-color"> Lorem Ipsum </span> features</h1>
+            <List className="list">
+                <ListItem className="list-item">
+                    <ListItemIcon>
+                        <CircleIcon className="list-item-icon" fontSize="" />
+                    </ListItemIcon>
+                    There are many variations of passages of Lorem Ipsum available
+                </ListItem><ListItem className="list-item">
+                    <ListItemIcon>
+                        <CircleIcon className="list-item-icon" fontSize="" />
+                    </ListItemIcon>
+                    Contrary to popular belief, Lorem Ipsum is not simply random text
+                </ListItem>
+                <ListItem className="list-item">
+                    <ListItemIcon>
+                        <CircleIcon className="list-item-icon" fontSize="" />
+                    </ListItemIcon>
+                    The standard chunk of Lorem Ipsum used since the 1500s is reproduced below for those interested.
+                </ListItem>
+                <ListItem className="list-item">
+                    <ListItemIcon>
+                        <CircleIcon className="list-item-icon" fontSize="" />
+                    </ListItemIcon>
+                    All the Lorem Ipsum generators on the Internet tend to repeat predefined chunks as necessary
+                </ListItem>
+                <ListItem className="list-item">
+                    <ListItemIcon>
+                        <CircleIcon className="list-item-icon" fontSize="" />
+                    </ListItemIcon>
+                    There is no one who loves pain itself, who seeks after it and wants to have it, simply because it is pain...
+                </ListItem>
+            </List>
+        </div> 
+        <div className="bottom-part">
+            <div className="blurred-part">
+                <div className="text-part">
+                    <h1 className="title-blurred">Lorem Ipsum</h1>
+                    <div className="text-blurred"> Various versions have evolved over the years, sometimes by accident, sometimes on purpose</div>
+                </div>
+                <img className="img-blurred" src={logo}></img>
+            </div>
+            <div className="bottom-text">
+                <h1 className="bottom-text-h1"> Come get some <span className="special-text-color-underlined">Lorem Ipsum</span> ! </h1>
+            </div>
+        </div>      
     </Container>
   );
 }
